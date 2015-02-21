19a6439219975bac11a4e754b6b1609d7a8b7b88
Richard Purves <franton@users.noreply.github.com>
2015-02-21 20:37:21 +0000

    Update munki_enroll.sh
    Modifications to allow passing of main catalog and manifest information to the php script that does the work. This is suitable for my current purposes.

--- Scripts/munki_enroll.sh
+++ Scripts/munki_enroll.sh
@@ -2,10 +2,14 @@
 
 # Script to enroll a computer to munki as part of a DeployStudio workflow.
 # Author  : contact@richard-purves.com
-# Version : 1.0 - Initial Version (based on https://github.com/flammable/munki-enroll)
+# Version : 21-02-2015 : 1.0 - Initial Version (based on https://github.com/flammable/munki-enroll)
+#         : This version has been modified to pass a custom catalog and manifest name to the php script.
+#         : Easier to mod this script than the php script after the fact.
 
 SUBMITURL="http://munki/repo/munki-enroll/enroll.php"
+CATALOG="production"
+MANIFEST="All Computers"
 
-/usr/bin/curl --max-time 5 --silent --get -d hostname=$DS_HOSTNAME $SUBMITURL
+/usr/bin/curl --max-time 5 --silent --get -d hostname=$DS_HOSTNAME catalog=$CATALOG manifest=$MANIFEST $SUBMITURL
 
 exit 0
