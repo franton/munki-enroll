bc8bc9238d6c2f06a0d878856b53237809a73816
Richard Purves <franton@users.noreply.github.com>
2015-02-21 20:31:02 +0000

    Update munki_enroll.sh

--- Scripts/munki_enroll.sh
+++ Scripts/munki_enroll.sh
@@ -1,12 +1,11 @@
 #!/bin/sh 
- 
-HOSTN=$( scutil --get LocalHostName );
+
+# Script to enroll a computer to munki as part of a DeployStudio workflow.
+# Author  : contact@richard-purves.com
+# Version : 1.0 - Initial Version (based on https://github.com/flammable/munki-enroll)
 
 SUBMITURL="http://munki/repo/munki-enroll/enroll.php"
 
-# Application paths
-CURL="/usr/bin/curl"
-
-$CURL --max-time 5 --silent --get -d hostname="$HOSTN" "$SUBMITURL"
+/usr/bin/curl --max-time 5 --silent --get -d hostname=$DS_HOSTNAME $SUBMITURL
 
 exit 0
