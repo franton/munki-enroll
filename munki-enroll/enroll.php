36c1efd6fdb773baa55ddcd5bd9328b3e403b703
Richard Purves <franton@users.noreply.github.com>
2015-02-21 20:34:10 +0000

    Update enroll.php
    Modified to take a specific catalog and manifest name from the script that calls this. I'm not a php expert so fingers crossed this works!

--- munki-enroll/enroll.php
+++ munki-enroll/enroll.php
@@ -4,6 +4,8 @@ require_once( 'cfpropertylist-1.1.2/CFPropertyList.php' );
 
 // Get the varibles passed by the enroll script
 $hostname   = $_GET["hostname"];
+$catalog    = $_GET["catalog"];
+$manifest   = $_GET["manifest"];
 
 // Check if manifest already exists for this machine
 if ( file_exists( '../manifests/' . $hostname ) )
@@ -20,12 +22,11 @@ else
         
         // Add manifest to release catalog by default
         $dict->add( 'catalogs', $array = new CFArray() );
-        $array->add( new CFString( 'release' ) );
+        $array->add( new CFString( $catalog ) );
         
         // Add manifests
         $dict->add( 'included_manifests', $array = new CFArray() );
-        $array->add( new CFString( '___core_software' ) );
-        $array->add( new CFString( '__faculty_staff' ) );
+        $array->add( new CFString( $manifest ) );
         
         // Save the newly created plist
         $plist->saveXML( '../manifests/' . $hostname );
